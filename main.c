e7090e00adf5ce9ab2b1a3735ebe921ca9f290aa
YasminCohen <yasminco123@gmail.com>
2022-12-06 12:21:34 +0200

    secend

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main()
     int stop = 1;
     while(stop != 0){
 
-        scanf(" Enter your chose %c " , &chose );
+        scanf(" Enter your chose  char %c " , &chose );
 
         if(chose == 'A'){
             scanMatrix();
@@ -20,12 +20,12 @@ int main()
 
         if(chose == 'B') {
             int p1, p2;
-            scanf("Enter your codcods %d %d ", &p1 ,&p2);
+            scanf("Insert the vertices you want to check which is the size of the shortest path between them:  %d %d ", &p1 ,&p2);
             shortestPath(p1, p2);
          }
         if (chose == 'C') {
             int V1, V2;
-            scanf("Enter your codcods %d %d ", &V1 ,&V2);
+            scanf("Insert the vertices you want to check if there is a route between them : %d %d ", &V1 ,&V2);
             isExistsPath(V1, V2);
         }
         if (chose == 'D') {
